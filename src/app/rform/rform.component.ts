42cc874ac0bc05a1e13c95db7980fb0136102eae
jangaprashanth880@gmail.com <you@example.com>
2023-07-31 14:28:33 +0530

    Random Changes

--- src/app/rform/rform.component.ts
+++ src/app/rform/rform.component.ts
@@ -8,17 +8,21 @@ import { FormControl,FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RformComponent {
 
-  constructor(private fb:FormBuilder){}
+  constructor(private fb: FormBuilder) { }
 
-  login=this.fb.group({
-    name:[null,[Validators.required],null],
-      email:[null,[Validators.required,Validators.email],null],
-      password: [null,[Validators.required,Validators.minLength(5)],null],
-      Confirmpassword: [null,[Validators.required],null]
+  login !: FormGroup;
+  
+  initUserForm(): void{
+  this.login = this.fb.group({
+    name: [null, [Validators.required], null],
+    email: [null, [Validators.required, Validators.email], null],
+    password: [null, [Validators.required, Validators.minLength(5)], null],
+    Confirmpassword: [null, [Validators.required], null]
 
   })
+}
 
-  show(r:any){
+  show(r: any) {
     console.log(r.value);
     r.reset();
   }
