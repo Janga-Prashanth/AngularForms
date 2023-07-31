4823c8102dd362b78397fd0072d20a44f2c60aab
jangaprashanth880@gmail.com <you@example.com>
2023-07-31 19:08:39 +0530

    Reactive form validators method changed

--- src/app/rform/rform.component.ts
+++ src/app/rform/rform.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl,FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,15 +6,19 @@ import { FormControl,FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './rform.component.html',
   styleUrls: ['./rform.component.css']
 })
-export class RformComponent {
+export class RformComponent implements OnInit{
 
   constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initUserForm();
+  }
 
   login !: FormGroup;
   
   initUserForm(): void{
   this.login = this.fb.group({
-    name: [null, [Validators.required], null],
+    name: ['', Validators.required],
     email: [null, [Validators.required, Validators.email], null],
     password: [null, [Validators.required, Validators.minLength(5)], null],
     Confirmpassword: [null, [Validators.required], null]
@@ -22,9 +26,18 @@ export class RformComponent {
   })
 }
 
-  show(r: any) {
-    console.log(r.value);
-    r.reset();
+  get f() {
+     return this.login.controls; 
+    }
+
+  onsubmit(r: any) {
+      if (this.login.invalid) {
+        return;
+    }
+    else{
+      console.log(r.value);
+      r.reset();
+    }    
   }
 
 }
